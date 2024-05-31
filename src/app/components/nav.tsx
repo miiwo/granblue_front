eeadc730dba33f31368c0438983a3f7a5e6b5610
koipoi <koipond575@gmail.com>
2024-05-31 12:52:58 -0400

    Add navbar

---
+++ src/app/components/nav.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+
+const navItems = {
+  '/': {
+    name: 'home',
+  },
+  '/calc': {
+    name: 'calculator',
+  },
+  '/charas': {
+    name: 'characters',
+  },
+  '/weapons': {
+    name: 'weapons',
+  },
+  'https://gbf.wiki/Main_Page': {
+    name: 'gbf.wiki',
+  },
+}
+
+const navDirectionLeft = 'pl-3'
+const navDirectionRight = 'justify-end'
+
+const navDirection = navDirectionRight
+
+export function Navbar() {
+  return (
+    <aside className="-ml-[8px] mb-16 tracking-tight">
+      <div className="lg:sticky lg:top-20">
+        <nav
+          className={`flex flex-row items-end relative ${navDirection} px-0 pb-0 pt-3 fade md:overflow-auto scroll-pr-6 md:relative`}
+          id="nav"
+        >
+          <div className="flex flex-row space-x-0 pr-10">
+            {Object.entries(navItems).map(([path, { name }]) => {
+              return (
+                <Link
+                  key={path}
+                  href={path}
+                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                >
+                  {name}
+                </Link>
+              )
+            })}
+          </div>
+        </nav>
+      </div>
+    </aside>
+  )
+}
