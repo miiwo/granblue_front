7b8eae98a2965eb5eceae24edb0336cf1dd1d45c
koipoi <koipond575@gmail.com>
2024-06-03 00:54:13 -0400

    Add a title

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -31,6 +31,7 @@ export function Navbar() {
           className={`flex flex-row items-end relative ${navDirection} px-0 pb-0 pt-3 fade md:overflow-auto scroll-pr-6 md:relative`}
           id="nav"
         >
+          <h1 className='flex-grow text-2xl pl-5 py-1 pr-2 m-1'>Skyfaring Domain</h1>
           <div className="flex flex-row space-x-0 pr-10">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
