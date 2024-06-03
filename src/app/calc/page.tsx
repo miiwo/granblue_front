a2172452ad2f1d63b74d7d20b755630e01f8e066
koipoi <koipond575@gmail.com>
2024-06-03 00:08:54 -0400

    Add damage mods table

--- src/app/calc/page.tsx
+++ src/app/calc/page.tsx
@@ -26,6 +26,10 @@ const formulaMods = {
     },
 } // Move this to gbfcalcContext later
 
+const strengthMods = {
+    'Crit': 25,
+    'Might': 125,
+}
 type SearchParamProps = {
     searchParams: Record<string, string> | null | undefined
 }
@@ -51,12 +55,12 @@ export default function CalculatorPage({ searchParams }: SearchParamProps) {
                     </div>
 
                     <div className="my-12">
-                        <h2>Summons</h2>
+                        <h2 className="font-bold text-xl mb-6">Summons</h2>
                         <SummonGrid />
                     </div>
 
                     <div className="my-12">
-                        <h2>Damage Formula</h2>
+                        <h2 className="font-bold text-xl mb-6">Damage Formula</h2>
                         <div className="flex flex-row gap-12 text-center">
                             {Object.entries(formulaMods).map(([modName, { num, mulx }]) => {
                                 return (
@@ -70,6 +74,21 @@ export default function CalculatorPage({ searchParams }: SearchParamProps) {
                                 )
                             })}
                         </div>
+                    </div>
+
+                    <div>
+                        <h2 className="font-bold text-xl mb-6">Damage Mods</h2>
+                        <table className="border-collapse table-auto w-3/4 text-left">
+                            <thead className="bg-slate-900 dark:border-slate-600 text-slate-400 dark:text-slate-400">
+                                <tr>
+                                    <th className="border-b">Mod Name</th>
+                                    <th className="border-b">Strength</th>
+                                </tr>
+                            </thead>
+                            <tbody className="bg-white border-slate-100 dark:bg-slate-700">
+                            {Object.keys(strengthMods).map(key => <tr key={key}><td className="border-b">{key}</td><td className="border-b">{String(strengthMods[key])}</td></tr>)}
+                            </tbody>
+                        </table>
                     </div>
 
                     {shouldDisplayWeaponModal && <GBFWepGridModal />}
