18dd8e90799103ae2b42452ab3d284e9f1f2a417
koipoi <koipond575@gmail.com>
2024-06-02 20:43:20 -0400

    Fix 11 weapons to be 10 weapons instead

--- src/app/components/calc/weapon-grid.tsx
+++ src/app/components/calc/weapon-grid.tsx
@@ -1,19 +1,22 @@
 'use client'
 
-import { GBFWeaponGridContext } from "@/app/calc/gbfcalcContext"
+import { GBFWeaponGridContext, Weapon } from "@/app/calc/gbfcalcContext"
 import { useContext } from "react"
 import { WeaponGridTile } from "./gbf_tile"
 
 export function WeaponGrid() {
 
     const { grid } = useContext(GBFWeaponGridContext)
+    let mainhand: Weapon | undefined, rest
+    ({ 'wepOne': mainhand, ...rest} = grid)
+    
     
     return (
         <div className="grid grid-rows-3 grid-cols-4 w-96">
             <div className="row-span-3">
-                <WeaponGridTile basepath="/calc" weapon={grid.wepOne} weaponlink="wepOne" />
+                <WeaponGridTile basepath="/calc" weapon={mainhand} weaponlink="wepOne" />
             </div>
-            {Object.keys(grid).map((keyName:string) => {
+            {Object.keys(rest).map((keyName:string) => {
                 return (
                     <WeaponGridTile key={keyName} weaponlink={keyName} basepath={"/calc"} weapon={grid[keyName]} />
                 )
