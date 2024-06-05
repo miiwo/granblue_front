a8f54e6f4a6096418d95804d50650ca92c779f44
koipoi <koipond575@gmail.com>
2024-06-05 13:42:41 -0400

    Sticky the footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <footer className="fixed bottom-0 z-20 bg-nordzero flex flex-col w-full">
+        <footer className="sticky bottom-0 z-20 bg-nordzero flex flex-col w-full">
             <span>© 2024 Skyfaring Domain</span>
             <span>Granblue Fantasy content and materials are trademarks and copyrights of Cygames, Inc. or its licensors. All rights reserved.</span>
         </footer>
