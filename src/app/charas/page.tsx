8504eeece0fce2b48bb73717fb858e1557a4e241
koipoi <koipond575@gmail.com>
2024-06-05 21:43:07 -0400

    Add an unimplemented page for characters

--- src/app/charas/page.tsx
+++ src/app/charas/page.tsx
@@ -1,3 +1,13 @@
+import Image from 'next/image'
+
 export default function CharactersPage() {
-    return <h1>Search for characters here</h1>
+    const imageChoices = ['/lyria_sorry.png', '/zephium.png']
+    const choice = Math.floor(Math.random() * imageChoices.length)
+
+    return (
+    <div className="flex flex-col justify-center items-center h-[80vh] gap-y-3">
+        <Image src={imageChoices[choice]} alt="Sorry picture" width={250} height={250} className='rounded' />
+        <p>NOT DONE YET, PLEASE CHECK IT OUT ANOTHER TIME</p>
+    </div>
+    )
 }
