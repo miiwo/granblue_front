d0610832dc529d045a938fe88fa6db941ea18f1d
koipoi <koipond575@gmail.com>
2024-06-05 22:06:05 -0400

    tabbing

--- src/app/charas/page.tsx
+++ src/app/charas/page.tsx
@@ -5,9 +5,9 @@ export default function CharactersPage() {
     const choice = Math.floor(Math.random() * imageChoices.length)
 
     return (
-    <div className="flex flex-col justify-center items-center h-[80vh] gap-y-3">
-        <Image src={imageChoices[choice]} alt="Sorry picture" width={250} height={250} className='rounded' />
-        <p>NOT DONE YET, PLEASE CHECK IT OUT ANOTHER TIME</p>
-    </div>
+        <div className="flex flex-col justify-center items-center h-[80vh] gap-y-3">
+            <Image src={imageChoices[choice]} alt="Sorry picture" width={250} height={250} className='rounded' />
+            <p>NOT DONE YET, PLEASE CHECK IT OUT ANOTHER TIME</p>
+        </div>
     )
 }
