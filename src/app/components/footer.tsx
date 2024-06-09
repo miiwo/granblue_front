46463f67056fc11c8cfe7c3a727fd9090c0271dd
koipoi <koipond575@gmail.com>
2024-06-09 13:45:24 -0400

    Adjust footer text

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
         <footer className="sticky top-[100vh] z-20 bg-nordzero flex flex-col w-full">
-            <span>© 2024 Skyfaring Domain</span>
+            {/*<span>© 2024 Skyfaring Domain</span>*/}
             <span>Granblue Fantasy content and materials are trademarks and copyrights of Cygames, Inc. or its licensors. All rights reserved.</span>
         </footer>
     )
