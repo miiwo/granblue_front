53d7aae36c347fc4632e01fc3f831cda59e9feb6
koipoi <koipond575@gmail.com>
2024-06-13 13:43:27 -0400

    Finalize responsive home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,16 @@ export default function Home() {
 
   return (
     <div className="h-screen grid grid-rows-3 justify-center">
-      <div className="row-span-1 text-center mx-4 md:mx-12 pt-14 md:pt-6">
+      <div className="row-span-1 text-center mx-4 md:mx-12 pt-14 md:pt-6 lg:px-56 lg:pt-20">
         <p>Hello and welcome! This is a website to calculate the mods in a grid, even if you don&apos;t have the weapon! It also holds a database of characters + weapon info as supplementary to the calculator.</p>
       </div>
       
-      <div className="flex flex-col place-items-center row-span-2 lg:w-96">
+      <div className="flex flex-col place-items-center row-span-2">
         <h1 className="mb-10 lg:mb-20 text-2xl">Latest Updates</h1>
         <div className="flex flex-col mx-3 md:flex-row gap-4 justify-center">
           {Object.keys(latestUpdates).map((key, i) => {
             return (
-              <span key={`latest_update_${i}`} className="border px-4 py-3 basis-1/3">
+              <span key={`latest_update_${i}`} className="border px-4 py-3 basis-1/3 lg:basis-none lg:w-[850px]">
                 <h2>{key}</h2>
                 <p>{latestUpdates[key]}</p>
               </span>
