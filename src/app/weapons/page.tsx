0ef02ee16d6920660ce82a4edef07cdd0617b47a
koipoi <koipond575@gmail.com>
2024-06-15 13:58:29 -0400

    Fix the active alignment on the search filters

--- src/app/weapons/page.tsx
+++ src/app/weapons/page.tsx
@@ -41,10 +41,10 @@ export default function WeaponPage() {
                             <label htmlFor="wep_name" className="w-1/3 z-10 cursor-pointer px-3 rounded-lg">Name</label>
 
                             <input id="wep_skill" type="radio" name="weapon_search_filters" value="skill" className='hidden peer/skill' />
-                            <label htmlFor='wep_skill' className="w-1/3 z-10 cursor-pointer ml-1 px-3 rounded-lg">Weapon Skill</label>
+                            <label htmlFor='wep_skill' className="w-1/3 z-10 cursor-pointer px-3 rounded-lg">Weapon Skill</label>
 
                             <input id="wep_ougi" type="radio" name="weapon_search_filters" value="ca" className='hidden peer/ca' />
-                            <label htmlFor='wep_ougi' className="w-1/3 z-10 cursor-pointer ml-1 px-3 rounded-lg select-none truncate">Ougi</label>
+                            <label htmlFor='wep_ougi' className="w-1/3 z-10 cursor-pointer px-3 rounded-lg select-none truncate">Ougi</label>
 
 
                             <div className='flex w-1/3 px-3 rounded-lg bg-nordred z-[9] h-full p-0 select-none truncate absolute transform transition-transform peer-checked/ca:translate-x-[200%] peer-checked/skill:translate-x-[100%]'></div>
@@ -58,24 +58,24 @@ export default function WeaponPage() {
                 <div className="flex flex-row items-center">
                     <span className='basis-32 text-right pr-5 bg-nordtwo'>Element:</span>
                     <div className='rounded overflow-x-auto'>
-                        <fieldset className='flex relative gap-3 rounded-lg bg-slate-700 lg:w-3/4 text-center'>
+                        <fieldset className='flex relative gap-3 rounded-lg bg-slate-700 lg:w-[820px] text-center'>
                             <input id="elementRadioFire" type="radio" name="elementRadio" value="Fire" defaultChecked className='hidden peer/Fire' />
                             <label htmlFor="elementRadioFire" className="w-1/6 z-10 cursor-pointer px-3 rounded-lg">Fire</label>
 
                             <input id="elementRadioWater" type="radio" name="elementRadio" value="Water" className='hidden peer/Water' />
-                            <label htmlFor='elementRadioWater' className="w-1/6 z-10 cursor-pointer ml-1 px-3 rounded-lg">Water</label>
+                            <label htmlFor='elementRadioWater' className="w-1/6 z-10 cursor-pointer px-3 rounded-lg">Water</label>
 
                             <input id="elementRadioEarth" type="radio" name="elementRadio" value="Earth" className='hidden peer/Earth' />
-                            <label htmlFor='elementRadioEarth' className="w-1/6 z-10 cursor-pointer ml-1 px-3 rounded-lg">Earth</label>
+                            <label htmlFor='elementRadioEarth' className="w-1/6 z-10 cursor-pointer px-3 rounded-lg">Earth</label>
 
                             <input id="elementRadioWind" type="radio" name="elementRadio" value="Wind" className='hidden peer/Wind' />
-                            <label htmlFor='elementRadioWind' className="w-1/6 z-10 cursor-pointer ml-1 px-3 rounded-lg">Wind</label>
+                            <label htmlFor='elementRadioWind' className="w-1/6 z-10 cursor-pointer px-3 rounded-lg">Wind</label>
 
                             <input id="elementRadioLight" type="radio" name="elementRadio" value="Light" className='hidden peer/Light' />
-                            <label htmlFor='elementRadioLight' className="w-1/6 z-10 cursor-pointer ml-1 px-3 rounded-lg">Light</label>
+                            <label htmlFor='elementRadioLight' className="w-1/6 z-10 cursor-pointer px-3 rounded-lg">Light</label>
 
                             <input id="elementRadioDark" type="radio" name="elementRadio" value="Dark" className='hidden peer/Dark' />
-                            <label htmlFor='elementRadioDark' className="w-1/6 z-10 cursor-pointer ml-1 px-3 rounded-lg">Dark</label>
+                            <label htmlFor='elementRadioDark' className="w-1/6 z-10 cursor-pointer px-3 rounded-lg">Dark</label>
 
 
                             <div className='flex w-1/6 px-3 rounded-lg bg-nordred z-[9] h-full p-0 select-none truncate absolute transform transition-transform peer-checked/Fire:translate-x-[0%] peer-checked/Water:translate-x-[100%] peer-checked/Earth:translate-x-[200%] peer-checked/Wind:translate-x-[300%] peer-checked/Light:translate-x-[400%] peer-checked/Dark:translate-x-[500%]'></div>
@@ -87,36 +87,36 @@ export default function WeaponPage() {
                 <div className="flex flex-row items-center">
                     <span className='basis-32 text-right pr-5 bg-nordtwo'>Weapon Type:</span>
                     <div className='rounded-lg overflow-x-auto'>
-                        <fieldset className='flex relative gap-3 rounded-lg bg-slate-700 lg:w-[85%] text-center place-items-center'>
+                        <fieldset className='flex relative rounded-lg bg-slate-700 gap-3 lg:w-[1200px] text-center place-items-center'>
                             <input id="wepTypeRadioSpear" type="radio" name="wepTypeRadio" value="Spear" defaultChecked className='hidden peer/Spear' />
                             <label htmlFor="wepTypeRadioSpear" className="w-[10%] z-10 cursor-pointer px-3 rounded-lg">Spear</label>
 
                             <input id="wepTypeRadioBow" type="radio" name="wepTypeRadio" value="Bow" className='hidden peer/Bow' />
-                            <label htmlFor='wepTypeRadioBow' className="w-[10%] z-10 cursor-pointer ml-1 px-3 rounded-lg">Bow</label>
+                            <label htmlFor='wepTypeRadioBow' className="w-[10%] z-10 cursor-pointer px-3 rounded-lg">Bow</label>
 
                             <input id="wepTypeRadioAxe" type="radio" name="wepTypeRadio" value="Axe" className='hidden peer/Axe' />
-                            <label htmlFor='wepTypeRadioAxe' className="w-[10%] z-10 cursor-pointer ml-1 px-3 rounded-lg">Axe</label>
+                            <label htmlFor='wepTypeRadioAxe' className="w-[10%] z-10 cursor-pointer px-3 rounded-lg">Axe</label>
 
                             <input id="wepTypeRadioDagger" type="radio" name="wepTypeRadio" value="Dagger" className='hidden peer/Dagger' />
-                            <label htmlFor='wepTypeRadioDagger' className="w-[10%] z-10 cursor-pointer ml-1 px-3 rounded-lg">Dagger</label>
+                            <label htmlFor='wepTypeRadioDagger' className="w-[10%] z-10 cursor-pointer px-3 rounded-lg">Dagger</label>
 
                             <input id="wepTypeRadioStaff" type="radio" name="wepTypeRadio" value="Staff" className='hidden peer/Staff' />
-                            <label htmlFor='wepTypeRadioStaff' className="w-[10%] z-10 cursor-pointer ml-1 px-3 rounded-lg">Staff</label>
+                            <label htmlFor='wepTypeRadioStaff' className="w-[10%] z-10 cursor-pointer px-3 rounded-lg">Staff</label>
 
                             <input id="wepTypeRadioMelee" type="radio" name="wepTypeRadio" value="Melee" className='hidden peer/Melee' />
-                            <label htmlFor='wepTypeRadioMelee' className="w-[10%] z-10 cursor-pointer ml-1 px-3 rounded-lg">Melee</label>
+                            <label htmlFor='wepTypeRadioMelee' className="w-[10%] z-10 cursor-pointer px-3 rounded-lg">Melee</label>
 
                             <input id="wepTypeRadioSword" type="radio" name="wepTypeRadio" value="Sword" className='hidden peer/Sword' />
-                            <label htmlFor='wepTypeRadioSword' className="w-[10%] z-10 cursor-pointer ml-1 px-3 rounded-lg">Sword</label>
+                            <label htmlFor='wepTypeRadioSword' className="w-[10%] z-10 cursor-pointer px-3 rounded-lg">Sword</label>
 
                             <input id="wepTypeRadioKatana" type="radio" name="wepTypeRadio" value="Katana" className='hidden peer/Katana' />
-                            <label htmlFor='wepTypeRadioKatana' className="w-[10%] z-10 cursor-pointer ml-1 px-3 rounded-lg">Katana</label>
+                            <label htmlFor='wepTypeRadioKatana' className="w-[10%] z-10 cursor-pointer px-3 rounded-lg">Katana</label>
 
                             <input id="wepTypeRadioHarp" type="radio" name="wepTypeRadio" value="Harp" className='hidden peer/Harp' />
-                            <label htmlFor='wepTypeRadioHarp' className="w-[10%] z-10 cursor-pointer ml-1 px-3 rounded-lg">Harp</label>
+                            <label htmlFor='wepTypeRadioHarp' className="w-[10%] z-10 cursor-pointer px-3 rounded-lg">Harp</label>
 
                             <input id="wepTypeRadioGun" type="radio" name="wepTypeRadio" value="Gun" className='hidden peer/Gun' />
-                            <label htmlFor='wepTypeRadioGun' className="w-[10%] z-10 cursor-pointer ml-1 px-3 rounded-lg">Gun</label>
+                            <label htmlFor='wepTypeRadioGun' className="w-[10%] z-10 cursor-pointer px-3 rounded-lg">Gun</label>
 
 
 
